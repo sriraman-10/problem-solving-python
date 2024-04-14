a3bf37ce31c48c7e805f6136e0ac7bc6ea242265
Sriraman Parthasarathi <sriraman10@Sriramans-MacBook-Pro.local>
2024-04-14 15:42:20 +0530

    max rectangle changes

--- problems/MaximumRectangle.py
+++ problems/MaximumRectangle.py
@@ -4,8 +4,8 @@ def maximalRectangle(self, matrix: list[list[str]]) -> int:
         m = len(matrix)
         n = len(matrix[0])
 
-        left = [0] * n # initialize left as the leftmost boundary possible
-        right = [n] * n # initialize right as the rightmost boundary possible
+        left = [0] * n
+        right = [n] * n
         height = [0] * n
 
         maxarea = 0
@@ -13,23 +13,19 @@ def maximalRectangle(self, matrix: list[list[str]]) -> int:
         for i in range(m):
 
             cur_left, cur_right = 0, n
-            # update height
             for j in range(n):
                 if matrix[i][j] == '1': height[j] += 1
                 else: height[j] = 0
-            # update left
             for j in range(n):
                 if matrix[i][j] == '1': left[j] = max(left[j], cur_left)
                 else:
                     left[j] = 0
                     cur_left = j + 1
-            # update right
             for j in range(n-1, -1, -1):
                 if matrix[i][j] == '1': right[j] = min(right[j], cur_right)
                 else:
                     right[j] = n
                     cur_right = j
-            # update the area
             for j in range(n):
                 maxarea = max(maxarea, height[j] * (right[j] - left[j]))
 
